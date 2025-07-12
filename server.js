5b7025910613fc62df1f2b9c515657b271ab55f5
S1nRTax <badreddine.asaad4@gmail.com>
2025-07-12 15:05:50 +0100

    setup the web server that serves the static files

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+
+
+const express = require('express'); // to create the web server.
+const http = require('http'); // Node.js built-in module for creating HTTP servers.
+const path = require('path'); // Node.js built-in module for working with file and directory paths.
+
+const app = express(); // creates an Express application instance
+const server = http.createServer(app); // creates an HTTP server, and passing the app to it.
+
+app.use(express.static(path.join(__dirname, 'public'))); // tells the server to server static files from Public directory. 
+
 console.log("Hello from server");
+
+const PORT = 6969; // set a port 
+server.listen(PORT, ()=> { // start the server listening on the port.
+	console.log(`Server is running at http://localhost:${PORT}`);
+	});
+
